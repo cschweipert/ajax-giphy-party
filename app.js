349a54a0a153f65cdd3adb35f49e9c10bce1d6e0
Chase Westover <chasejwestover@gmail.com>
2021-02-23 18:25:18 -0600

    established working server

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 console.log("Let's get this party started!");
 
-function 
+//function to GET from giphy
+async function getFromGiphy(evt){
+    evt.preventDefault();
+    let formInput = $("#input").val();
+    let giph = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=E3r6peC1o6A2DJ29U3mxJk8SUeoLDAeO&q=${formInput}`);
+    let dataLength = giph.data.data.length;
+    let randomIdx = Math.floor(Math.random() * dataLength);
+    $("#meme_container").append( $("<img>").attr("src", giph.data.data[randomIdx].images.original.url));
+}
+
+
+async function removeAllGifs(){
+    $("#meme_container").empty();
+}
+
+//event listener for the submit button
+$("form").on("submit",getFromGiphy);
+$("#remove").on("click", removeAllGifs);
